70e2c5e04ab35bfd5a895cbd90c97f4a092e07c9
VictorCher <192.168.127.1@mail.ru>
2019-02-20 22:17:04 +0300

    Начал делать задание 1

---
+++ CSharp_level2/BaseSalary.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGame
+{
+    abstract class BaseSalary
+    {
+        protected double salary;
+
+        protected BaseSalary()
+        {
+            //this.salary = rate;
+        }
+        public double Salary => salary;
+    }
+}
---
+++ CSharp_level2/HourlySalary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGame
+{
+    class HourlySalary : BaseSalary
+    {
+        public HourlySalary(double rate)
+        {
+            this.salary = 20.8 * 8 * rate;
+        }
+    }
+}
