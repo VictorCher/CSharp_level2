7b011f54fb58cce3908de87b49c75c3043b97997
Виктор <@SL-SERVISA>
2019-03-19 16:35:04 +0300

    Написал клиентскую часть

--- CSharp_level2_WebAPI/App_Start/WebApiConfig.cs
+++ CSharp_level2_WebAPI/App_Start/WebApiConfig.cs
@@ -19,6 +19,7 @@ namespace CSharp_level2_WebAPI
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+            
         }
     }
 }
---
+++ WebAPI_Client/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace WebAPI_Client
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Department { get; set; }
+    }
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage result = client.GetAsync(@"http://localhost:55535/api/employees/").Result;
+            var users = result.Content.ReadAsAsync<IEnumerable<Employee>>();  
+        }
+    }
+}
